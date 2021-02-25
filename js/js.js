d558b23621d7a7468847156dfebadb6800631659
ChaEma7 <70863947+ChaEma7@users.noreply.github.com>
2021-02-25 09:02:35 +0100

    vision 1 - Chalotte

--- js/js.js
+++ js/js.js
@@ -2,5 +2,38 @@
  * file: js.js
  * purpose: interactivity
  **/
-console.log('JavaScript from js/js.js: up and running!');
+console.log('JavaScript from js/js.js: up and running!')
 
+/* forsøg på at lave en scroll animation */
+
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animationScroll(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+
+var section1 = document.querySelector('.vider-knap-1');
+
+section1.addEventListener('click', function () {
+    smoothScroll('.section-2', 1000)
+});
